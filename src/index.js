0571c7fdb5227b4f018ad38ce500ff8eba44e67d
Dailyscat <pay_nack@naver.com>
2019-11-07 01:36:32 +0900

    appsync config와 app 렌더링 connect

--- src/index.js
+++ src/index.js
@@ -4,9 +4,36 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
 
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from "aws-appsync-react";
+import { ApolloProvider } from "react-apollo";
+
+import { AppSync } from "./appsync.json";
+console.log(AppSync);
+let appSyncConfig = AppSync.Default;
+
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.ApiUrl,
+  region: appSyncConfig.Region,
+  auth: {
+    type: appSyncConfig.AuthMode,
+    apiKey: appSyncConfig.ApiKey
+  }
+});
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={client}>
+      <Rehydrated
+        render={({ rehydrated }) =>
+          rehydrated ? (
+            <App />
+          ) : (
+            <strong>Your custom UI componen here...</strong>
+          )
+        }
+      ></Rehydrated>
+    </ApolloProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
